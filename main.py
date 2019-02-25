70543c77454390c4b9c08bec867f67befb06fff8
juliarosenrauch <jul_rose@yahoo.ca>
2019-02-25 14:32:41 -0800

    Turned on fast recording

--- main.py
+++ main.py
@@ -46,41 +46,41 @@ if __name__ == "__main__":
         return sum(list)/len(list)
 
     while True:
-        # ***** SLOW *****
-        if dig1.get_dig_value():
-            print("At time," , time.monotonic(), ", my reading is," , adc1.value_to_voltage())
-        else:
-            continue
-
-
-        # # ***** FAST *****
-        #
+        # # ***** SLOW *****
         # if dig1.get_dig_value():
-        #     # here u have another if statement that only takes readings when the readings are above the baseline and then stops when they fall back to within a 0.1V range of the baseline
-        #     if (adc1.value_to_voltage() > (average(baseline))):
-        #         timeRead.append(time.monotonic())
-        #         data.append(adc1.value_to_voltage())
-        #         # ii = 0
-        #         # while (ii<0xFF):
-        #         #     ii += 1
-        #     else:
-        #         continue
-        #
+        #     print("At time," , time.monotonic(), ", my reading is," , adc1.value_to_voltage())
         # else:
-        #     # here u toggle the second switch and take like 100 measurements, average them, and that becomes your baseline
-        #     if dig2.get_dig_value():
-        #         while (len(baseline) < 100):
-        #             baseline.append(adc1.value_to_voltage())
-        #     else:
-        #         if (len(timeRead) > 0):
-        #             for i in range(1, len(timeRead)):
-        #                 print(("At time, %12.5f, my reading is, %f") % (timeRead[i], data[i]))
-        #                 time.sleep(0.01)
-        #             print("== DONE ==")
-        #             print ("baseline average is:", average(baseline))
-        #                 # print("At time," , timeRead[i], ", my reading is," , data[i])
-        #             timeRead = []
-        #             data = []
-        #             baseline = []
-        #         else:
-        #             continue
+        #     continue
+
+
+        # ***** FAST *****
+
+        if dig1.get_dig_value():
+            # here you have another if statement that only takes readings when the readings are above the baseline and then stops when they fall back to within a 0.1V range of the baseline
+            if (adc1.value_to_voltage() > (average(baseline))):
+                timeRead.append(time.monotonic())
+                data.append(adc1.value_to_voltage())
+                # ii = 0
+                # while (ii<0xFF):
+                #     ii += 1
+            else:
+                continue
+
+        else:
+            # here you toggle the second switch and take 100 measurements, average them, and that becomes your baseline
+            if dig2.get_dig_value():
+                while (len(baseline) < 100):
+                    baseline.append(adc1.value_to_voltage())
+            else:
+                if (len(timeRead) > 0):
+                    for i in range(1, len(timeRead)):
+                        print(("At time, %12.5f, my reading is, %f") % (timeRead[i], data[i]))
+                        time.sleep(0.01)
+                    print("== DONE ==")
+                    print ("baseline average is:", average(baseline))
+                        # print("At time," , timeRead[i], ", my reading is," , data[i])
+                    timeRead = []
+                    data = []
+                    baseline = []
+                else:
+                    continue
